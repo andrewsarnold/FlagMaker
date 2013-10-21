6b563fe4b6e58506fd990ecc85ffff1fe28587e1
Andrew Arnold <asa@hmbnet.com>
2013-10-21 00:58:30 -0400

    click-to-edit slider values for overlays

--- FlagMaker/Overlays/AttributeSlider.xaml.cs
+++ FlagMaker/Overlays/AttributeSlider.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Security;
 using System.Windows;
 using System.Windows.Controls.Primitives;
+using System.Windows.Input;
 
 namespace FlagMaker.Overlays
 {
@@ -24,6 +26,7 @@ namespace FlagMaker.Overlays
 			Slider.Value = value;
 			Slider.TickFrequency = 1;
 			Slider.TickPlacement = TickPlacement.BottomRight;
+			TxtValue.Visibility = Visibility.Hidden;
 		}
 
 		public int Maximum
@@ -42,7 +45,7 @@ namespace FlagMaker.Overlays
 			{
 				Slider.Value = value;
 			}
-	}
+		}
 
 		private void SliderValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
 		{
@@ -63,6 +66,38 @@ namespace FlagMaker.Overlays
 			{
 				Slider.Value = (int) Math.Round(Slider.Value, 0);
 			}
+		}
+
+		private void Clicked(object sender, MouseButtonEventArgs e)
+		{
+			TxtValue.Visibility = Visibility.Visible;
+			TxtValue.Text = Slider.Value.ToString();
+			TxtValue.SelectAll();
+			TxtValue.Focus();
+		}
+
+		private void TxtValueKeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.Key == Key.Enter)
+			{
+				TxtValue.Visibility = Visibility.Hidden;
+
+				double value;
+				if (double.TryParse(TxtValue.Text, out value))
+				{
+					chkDiscrete.IsChecked = ((int) value == value);
+					Slider.Value = value;
+				}
+			}
+			else if (e.Key == Key.Escape)
+			{
+				TxtValue.Visibility = Visibility.Hidden;
+			}
+		}
+
+		private void TxtValueLostFocus(object sender, RoutedEventArgs e)
+		{
+			TxtValue.Visibility = Visibility.Hidden;
 		}
 	}
 }
