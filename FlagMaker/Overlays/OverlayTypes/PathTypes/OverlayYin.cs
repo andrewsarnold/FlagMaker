aa5de1513e78a7cc0f3dc43b66174b9f36f5f1b1
Andrew Arnold <asa@hmbnet.com>
2015-01-05 20:23:04 -0500

    improved yin & yang overlays

--- FlagMaker/Overlays/OverlayTypes/PathTypes/OverlayYin.cs
+++ FlagMaker/Overlays/OverlayTypes/PathTypes/OverlayYin.cs
@@ -5,8 +5,8 @@ namespace FlagMaker.Overlays.OverlayTypes.PathTypes
 {
 	internal sealed class OverlayYin : OverlayPath
 	{
-		private const string Path = "M -21.643592,-14.393365 A 12.993263,12.993263 0 1 0 -0.02149594,0.02136686 12.993263,12.993263 0 0 1 21.600599,14.436096 25.986525,25.986525 0 0 1 -21.643592,-14.393365 z";
-		private static readonly Vector PathSize = new Vector(56, 45);
+		private const string Path = "M 83.20869,-55.472461 A 50.0022,50.0022 0 0 1 0,0 50.0022,50.0022 0 1 0 -83.20869,55.472461 100.0044,100.0044 0 1 0 83.20869,-55.472461 z";
+		private static readonly Vector PathSize = new Vector(200, 200);
 
 		public OverlayYin(int maximumX, int maximumY)
 			: base("yin", Path, PathSize, maximumX, maximumY)
