253667ee77d8e9b25e0c6d72614e51e1098d1001
Andrew Arnold <andrew.s.arnold@gmail.com>
2015-03-25 19:18:10 -0400

    Polish translation

--- FlagMaker/MainWindow.xaml.cs
+++ FlagMaker/MainWindow.xaml.cs
@@ -95,6 +95,7 @@ namespace FlagMaker
 				new CultureInfo("en-US"),
 				new CultureInfo("es-ES"),
 				new CultureInfo("fr-FR"),
+				new CultureInfo("pl-PL"),
 				new CultureInfo("ru-RU")
 			}.Select(lang => new MenuItem
 			{
